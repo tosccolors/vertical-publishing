e3a5513da3193ba529878ee21e8041dade2f5bc4
praveen-kumarG <praveenkg@systemsvalley.co.in>
2018-02-16 11:00:44 +0530

    Task: Links to advertising order go to regular order views.

--- sale_advertising_order/models/res_config.py
+++ sale_advertising_order/models/res_config.py
@@ -27,6 +27,33 @@ class Partner(models.Model):
 
     agency_discount = fields.Float('Agency Discount (%)', digits=(16, 2), default=0.0)
     is_ad_agency = fields.Boolean('Agency', default=False)
+    adv_sale_order_count = fields.Integer(compute='_compute_adv_sale_order_count', string='# of Sales Order')
+
+    def _compute_adv_sale_order_count(self):
+        sale_data = self.env['sale.order'].read_group(domain=[('partner_id', 'child_of', self.ids),('advertising','=',True),('state','in',('sale','done'))],
+                                                      fields=['partner_id'], groupby=['partner_id'])
+        # read to keep the child/parent relation while aggregating the read_group result in the loop
+        partner_child_ids = self.read(['child_ids'])
+        mapped_data = dict([(m['partner_id'][0], m['partner_id_count']) for m in sale_data])
+        for partner in self:
+            # let's obtain the partner id and all its child ids from the read up there
+            partner_ids = filter(lambda r: r['id'] == partner.id, partner_child_ids)[0]
+            partner_ids = [partner_ids.get('id')] + partner_ids.get('child_ids')
+            # then we can sum for all the partner's child
+            partner.adv_sale_order_count = sum(mapped_data.get(child, 0) for child in partner_ids)
+
+    def _compute_sale_order_count(self):
+        sale_data = self.env['sale.order'].read_group(domain=[('partner_id', 'child_of', self.ids),('state','not in',('draft','sent','cancel')), ('advertising','=',False)],
+                                                      fields=['partner_id'], groupby=['partner_id'])
+        # read to keep the child/parent relation while aggregating the read_group result in the loop
+        partner_child_ids = self.read(['child_ids'])
+        mapped_data = dict([(m['partner_id'][0], m['partner_id_count']) for m in sale_data])
+        for partner in self:
+            # let's obtain the partner id and all its child ids from the read up there
+            partner_ids = filter(lambda r: r['id'] == partner.id, partner_child_ids)[0]
+            partner_ids = [partner_ids.get('id')] + partner_ids.get('child_ids')
+            # then we can sum for all the partner's child
+            partner.sale_order_count = sum(mapped_data.get(child, 0) for child in partner_ids)
 
     def name_get_custom(self, partner_ids):
         if not partner_ids:
