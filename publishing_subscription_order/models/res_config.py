c4e6dce700484d56d476e47038402c6e17a128fa
kumarinie <kumarisushmanie@gmail.com>
2018-05-30 18:48:44 +0530

    Subscription Delivery List

--- publishing_subscription_order/models/res_config.py
+++ publishing_subscription_order/models/res_config.py
@@ -7,8 +7,6 @@ class AccountConfigSettings(models.TransientModel):
 
     delivery_obligation_account_id = fields.Many2one('account.account', string="Delivery Obligation Account", domain=[('deprecated', '=', False)])
     subscription_revenue_account_id = fields.Many2one('account.account', string='Subscription Revenue Account', domain=[('deprecated', '=', False)])
-    susbcription_journal_id = fields.Many2one('account.journal', string="Subscription Journal")
-
 
     @api.onchange('company_id')
     def onchange_company_id(self):
@@ -18,10 +16,8 @@ class AccountConfigSettings(models.TransientModel):
             ir_values = self.env['ir.values']
             delivery_account_id = ir_values.get_default('product.template', 'delivery_obligation_account_id', company_id = self.company_id.id)
             revenue_account_id = ir_values.get_default('product.template', 'subscription_revenue_account_id', company_id = self.company_id.id)
-            susbcription_journal_id = ir_values.get_default('subscription.prepaid', 'susbcription_journal_id', company_id = self.company_id.id)
             self.delivery_obligation_account_id = delivery_account_id if delivery_account_id else False
             self.subscription_revenue_account_id = revenue_account_id if revenue_account_id else False
-            self.susbcription_journal_id = susbcription_journal_id if susbcription_journal_id else False
         return res
 
     @api.multi
@@ -30,4 +26,3 @@ class AccountConfigSettings(models.TransientModel):
         ir_values_obj = self.env['ir.values']
         ir_values_obj.sudo().set_default('product.template', "delivery_obligation_account_id", self.delivery_obligation_account_id.id if self.delivery_obligation_account_id else False, for_all_users=True, company_id=self.company_id.id)
         ir_values_obj.sudo().set_default('product.template', "subscription_revenue_account_id", self.subscription_revenue_account_id.id if self.subscription_revenue_account_id else False, for_all_users=True, company_id=self.company_id.id)
-        ir_values_obj.sudo().set_default('subscription.prepaid', "susbcription_journal_id", self.susbcription_journal_id.id if self.susbcription_journal_id else False, for_all_users=True, company_id=self.company_id.id)
