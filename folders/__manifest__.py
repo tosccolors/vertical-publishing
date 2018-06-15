358143a4eb8255368be0e48f6da7395717951ddc
praveen-kumarG <praveenkg@systemsvalley.co.in>
2018-06-15 15:43:42 +0530

    Updated folders module with copyright and access right.

--- folders/__manifest__.py
+++ folders/__manifest__.py
@@ -1,22 +1,28 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2016 Magnus (<http://www.magnus.nl>). All Rights Reserved
-#    $Id$
+# Copyright (c) 2013 Megis - Willem Hulshof - www.megis.nl
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs.
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company like Veritos.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #
 ##############################################################################
 {
@@ -36,6 +42,7 @@ specific logistics service provider.
     'depends': ['base', 'sale', 'sale_advertising_order'],
     'data': [
         "views/logistics_addres_table_view.xml",
+        "security/ir.model.access.csv"
     ],
     'installable': True
 }
