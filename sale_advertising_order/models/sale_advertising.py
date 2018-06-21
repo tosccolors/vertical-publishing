318fa6b1a430c8a2adb271c741d0b3e9bfea1a4c
Willem Hulshof <w.hulshof@magnus.nl>
2018-06-21 14:05:24 +0200

    SAO change order_line when status = invoiced: allow when admin and group_invoicing

--- sale_advertising_order/models/sale_advertising.py
+++ sale_advertising_order/models/sale_advertising.py
@@ -995,7 +995,8 @@ class SaleOrderLine(models.Model):
             if 'pubble_sent' in vals:
                 continue
             if line.invoice_status == 'invoiced' and not vals.get('product_uom_qty') == 0 \
-                                                 and not user.has_group('account.group_account_invoice'):
+                                                 and not user.has_group('account.group_account_invoice') \
+                                                 and not user.id == 1:
                 raise UserError(_('You cannot change an order line after it has been fully invoiced.'))
             if not line.multi_line and ('product_id' in vals or 'adv_issue' in vals or 'product_uom_qty' in vals):
                 if line.deadline_check():
