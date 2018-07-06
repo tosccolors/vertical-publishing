6f6e0b5b477f8fc16739d1bd799088ffd06afa4f
Willem Hulshof <w.hulshof@magnus.nl>
2018-07-06 09:06:50 +0200

    revert1 reverted

--- sale_advertising_order/models/crm_lead.py
+++ sale_advertising_order/models/crm_lead.py
@@ -357,16 +357,16 @@ class Lead(models.Model):
             ('state', 'in', ['sale', 'done']),
             ('user_id', '=', self.env.uid),
         ]
-        sale_data = self.env['sale.order'].search_read(sale_order_domain, ['confirmation_date', 'amount_untaxed'])
+
+        sale_data = self.env['sale.order'].search_read(sale_order_domain, ['date_order', 'amount_untaxed'])
         for sale in sale_data:
-            if sale['confirmation_date']:
-                sale_date = fields.Datetime.from_string(sale['confirmation_date'])
+            if sale['date_order']:
+                sale_date = fields.Datetime.from_string(sale['date_order'])
                 if sale_date <= current_datetime and sale_date >= current_datetime.replace(day=1):
                     result['sale_confirmed']['this_month'] += sale['amount_untaxed']
                 elif sale_date < current_datetime.replace(day=1) and sale_date >= current_datetime.replace(day=1) - relativedelta(months=+1):
                     result['sale_confirmed']['last_month'] += sale['amount_untaxed']
         result['invoiced']['target'] = self.env.user.target_sales_invoiced
-
         result['reg_quotes'] = {'overdue': 0}
         result['adv_quotes'] = {'overdue': 0}
         quote_domain = [
@@ -417,6 +417,17 @@ class Lead(models.Model):
 class Team(models.Model):
     _inherit = ['crm.team']
 
+
+    @api.multi
+    def _compute_invoiced(self):
+        for team in self:
+            confirmed_sales = self.env['sale.order'].search([
+                ('state', 'in', ['sale', 'done']),
+                ('team_id', '=', team.id),
+                ('date_order', '<=', datetime.now().strftime('%Y-%m-%d %H:%M:%S')),
+                ('date_order', '>=', datetime.now().replace(day=1).strftime('%Y-%m-%d %H:%M:%S')),
+            ])
+            team.invoiced = sum(confirmed_sales.mapped('amount_untaxed'))
 
     @api.model
     def action_your_pipeline(self):
