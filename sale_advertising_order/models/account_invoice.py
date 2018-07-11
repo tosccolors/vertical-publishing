c7026d089d23dbc9030392c8f174ad117a88ab2b
Willem Hulshof <w.hulshof@magnus.nl>
2018-07-11 15:48:06 +0200

    correction Invoice editable: nett_nett out and no computing of discount from partner anymore

--- sale_advertising_order/models/account_invoice.py
+++ sale_advertising_order/models/account_invoice.py
@@ -31,7 +31,6 @@ class Invoice(models.Model):
     ad = fields.Boolean(related='invoice_line_ids.ad', string='Ad', help="It indicates that the invoice is an Advertising Invoice.", store=True)
     magazine = fields.Boolean(related='invoice_line_ids.so_line_id.magazine', string='Magazine', readonly=True, store=True)
     published_customer = fields.Many2one('res.partner', 'Advertiser', domain=[('customer', '=', True)])
-    nett_nett = fields.Boolean(string='Nett Nett')
 
 
 class InvoiceLine(models.Model):
@@ -39,33 +38,29 @@ class InvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
 
     @api.one
-    @api.depends('quantity', 'discount', 'invoice_id.partner_id', 'invoice_line_tax_ids', 'invoice_id.nett_nett',
-                 'nett_nett', 'price_unit', 'product_id', 'invoice_id.currency_id', 'invoice_id.company_id',
-                 'invoice_id.date_invoice', 'invoice_id.date')
+    @api.depends('price_unit', 'discount', 'invoice_line_tax_ids', 'quantity',
+        'product_id', 'invoice_id.partner_id', 'invoice_id.currency_id', 'invoice_id.company_id',
+        'invoice_id.date_invoice', 'invoice_id.date', 'ad')
     def _compute_price(self):
         """
         Compute subtotal_before_agency_disc.
         """
+        super(InvoiceLine, self)._compute_price()
         if self.filtered('ad'):
-            nn = True if self.invoice_id.nett_nett or self.nett_nett else False
             price_unit = self.price_unit or 0.0
             qty = self.quantity or 0.0
-            if self.invoice_id.partner_id.is_ad_agency or self.invoice_id.partner_id.parent_id.is_ad_agency and not nn:
-                self.discount = self.invoice_id.partner_id.agency_discount or self.invoice_id.partner_id.parent_id.agency_discount
-            else:
-                self.discount = 0.0
             if price_unit and qty:
                 self.subtotal_before_agency_disc = price_unit * qty
-        super(InvoiceLine, self)._compute_price()
+        else:
+            self.subtotal_before_agency_disc = 0.0
 
 
     date_publish = fields.Date('Publishing Date')
     so_line_id = fields.Many2one('sale.order.line', 'link between Sale Order Line and Invoice Line')
     computed_discount = fields.Float(string='Discount' )
-    subtotal_before_agency_disc = fields.Float(compute='_compute_price', string='SBAD' )
+    subtotal_before_agency_disc = fields.Float(compute='_compute_price', string='SBAD', readonly=True )
     ad_number = fields.Char(string='External Reference')
     opportunity_subject = fields.Char(string='Subject')
-    nett_nett = fields.Boolean(string='Nett Nett')
     sale_order_id = fields.Many2one(related='so_line_id.order_id', relation='sale.order', store=True, string='Order Nr.')
     ad = fields.Boolean(related='so_line_id.advertising', string='Ad', store=True,
                                 help="It indicates that the invoice line is from an Advertising Invoice.")
