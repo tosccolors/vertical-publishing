3d0da8b70c21798c702f6306ee038d1d8b5502e5
Willem Hulshof <w.hulshof@magnus.nl>
2018-11-29 23:04:35 +0100

    removal of subscription_baarn

--- subscription_baarn/models/res_partner.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class Partner(models.Model):
-    _inherit = 'res.partner'
-
-    wijknummer = fields.Char('Wijknummer')
-    even_oneven = fields.Char('Even-Oneven')
