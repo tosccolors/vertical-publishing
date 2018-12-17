aa8e2ccd15f4666b56384e386361e99934c258c1
Stephan van de Berg <berg.stephan@gmail.com>
2018-12-17 18:07:56 +0100

    No dependency required with BDU account

--- package_sale_advertising_order/__manifest__.py
+++ package_sale_advertising_order/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['sale_advertising_order','bdu_account'],
+    'depends': ['sale_advertising_order'],
 
     # always loaded
     'data': [
