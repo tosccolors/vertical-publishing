3efb17eda5944b2550d38a7c3c83dd5a7384bbf2
Dhrupesh <kotadiyadhrupesh@gmail.com>
2019-07-19 14:17:51 +0530

    T0096 - Show from_date and to_date as new column in advertising sale order one2many view

---
+++ sale_advertising_order_orderline/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Willem Hulshof Magnus (www.magnus.nl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': "sale_advertising_order_orderline",
+
+    'summary': "This module help to add field in sale order line",
+
+    'description': """
+        Display from_date and to_date in sale order line
+    """,
+
+    'author': "DK-Magnus",
+    'website': "http://www.magnus.nl",
+
+    'category': 'Sale',
+    'version': '0.1',
+    'depends': ['sale_advertising_order'],
+    'data': ['views/sale_advertising_view.xml'],
+}
