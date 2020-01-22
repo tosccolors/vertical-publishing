33dd232fbcb8bfe73053024a5ae39951be102bd3
kumarinie <kumarisushmanie@gmail.com>
2020-01-22 18:54:51 +0530

    Task: T0420 - Remove date mapping for purchase order

--- advertising_order_print_deferred_costs/models/account_invoice.py
+++ advertising_order_print_deferred_costs/models/account_invoice.py
@@ -2,21 +2,21 @@
 
 from odoo import models, fields, api
 
-class AccountInvoice(models.Model):
-    _inherit = "account.invoice"
+# class AccountInvoice(models.Model):
+#     _inherit = "account.invoice"
 
-    def _prepare_invoice_line_from_po_line(self, line):
-        """
-        :param line:
-        :return: Update start_date & end_date from advertising.issue looking into PO line account_analytic_id
-        """
-        data = super(AccountInvoice, self)._prepare_invoice_line_from_po_line(line)
-        if line.account_analytic_id:
-            adv_issue = self.env['sale.advertising.issue'].search(
-                [('analytic_account_id', '=', line.account_analytic_id.id)])
-            issue_date = adv_issue.issue_date if len(adv_issue) == 1 else False
-            data['start_date'] = data['end_date'] = issue_date
-        return data
+    # def _prepare_invoice_line_from_po_line(self, line):
+    #     """
+    #     :param line:
+    #     :return: Update start_date & end_date from advertising.issue looking into PO line account_analytic_id
+    #     """
+    #     data = super(AccountInvoice, self)._prepare_invoice_line_from_po_line(line)
+    #     if line.account_analytic_id:
+    #         adv_issue = self.env['sale.advertising.issue'].search(
+    #             [('analytic_account_id', '=', line.account_analytic_id.id)])
+    #         issue_date = adv_issue.issue_date if len(adv_issue) == 1 else False
+    #         data['start_date'] = data['end_date'] = issue_date
+    #     return data
 
 class AccountInvoiceLine(models.Model):
     _inherit = "account.invoice.line"
