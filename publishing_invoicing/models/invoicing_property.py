eb77799ed289905b74b8e63d081c3dfbcb117a03
DeekshithSystemsValley <deekshith@systemsvalley.co.in>
2020-11-05 14:19:47 +0530

    T0455-Modify advertising invoicing- added publishing_invoicing with latest updates

--- publishing_invoicing/models/invoicing_property.py
+++ publishing_invoicing/models/invoicing_property.py
@@ -3,13 +3,34 @@
 from odoo import models, fields, api
 
 class nsm_invoicing_property(models.Model):
-    _name = 'invoicing.property'
+	_name = 'invoicing.property'
 
-    name = fields.Char(string="Invoicing Property")
-    
-    group_invoice_lines_per_period = fields.Boolean(string="Group invoice lines per period")
-    group_invoice_lines_per_title = fields.Boolean(string="Group invoice lines per title")
-    group_invoice_lines_per_po_number = fields.Boolean(string="Group invoice lines per PO number")
+	name = fields.Char(string="Invoicing Property")
+	
+	group_invoice_lines_per_period = fields.Boolean(string="Group invoice lines per period")
+	group_invoice_lines_per_title = fields.Boolean(string="Group invoice lines per title")
+	group_invoice_lines_per_po_number = fields.Boolean(string="Group invoice lines per PO number")
+	only_invoice_published_adv = fields.Boolean(string="Only invoice published advertisements")
+	group_by_week = fields.Boolean(string="Group by Week")
+	group_by_month = fields.Boolean(string="Group by Month")
+	pre_pay_publishing_add = fields.Boolean(string="Group by pre-pay publishing advertisements")
+	pre_pay_online_add = fields.Boolean(string="Group by pre-pay online advertisements")
+	# remove above
+	group_by_edition = fields.Boolean(string="Group by Edition")
+	group_by_order = fields.Boolean(string="Group by order")
+	group_by_advertiser = fields.Boolean(string="Group by Advertiser")
+	group_by_online_separate = fields.Boolean(string="Group by online / print separate")
 
-    pre_pay_publishing_add = fields.Boolean(string="Pre-pay publishing advertisements")
-    pre_pay_online_add = fields.Boolean(string="Pre-pay online advertisements")
+
+	# newly added
+	inv_package_deal = fields.Boolean(string="Invoice package deal")
+	inv_per_line_adv_print = fields.Boolean(string="Invoice per OrderLine in advance print")
+	inv_per_line_after_online = fields.Boolean(string="Invoice per OrderLine afterwards online")
+	inv_whole_order_at_once = fields.Boolean(string="Invoice whole order at once")
+	pay_in_terms = fields.Boolean(string="Pay in terms")
+
+	inv_per_line_after_print = fields.Boolean(string="Invoice per orderline afterwards print")
+	inv_per_line_adv_online = fields.Boolean(string="Invoice per orderline in advance online")
+
+
+	
--- publishing_invoicing/models/sale_order.py
+++ publishing_invoicing/models/sale_order.py
@@ -2,14 +2,99 @@
 from odoo import api, fields, models, _
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+	_inherit = 'sale.order'
 
-    invoicing_property_id = fields.Many2one('invoicing.property',string="Invoicing Property")
+	invoicing_property_id = fields.Many2one('invoicing.property',string="Invoicing Property",required=True)
+	invoicing_date = fields.Date(string="Invoicing Date")
+	inv_date_bool = fields.Boolean(string="Set attribute to Invoicing date field")
+	terms_condition = fields.Text(string="Terms and condition")
+	terms_cond_bool = fields.Boolean(string="Set attribute to Terms & condition field")
 
-    @api.multi
-    @api.onchange('partner_id')
-    def update_invoicing_property(self):
-        for line in self:
-            if line.partner_id:
-                if line.partner_id.invoicing_property_id:
-                    line.invoicing_property_id = line.partner_id.invoicing_property_id.id
+	@api.multi
+	@api.onchange('published_customer','advertising_agency')
+	def onchange_customer_publishing_invoicing(self):
+		for line in self:
+			if line.advertising_agency:
+				if line.advertising_agency.invoicing_property_id:
+					line.invoicing_property_id = line.advertising_agency.invoicing_property_id.id
+			else:
+				if line.published_customer.invoicing_property_id:
+					line.invoicing_property_id = line.published_customer.invoicing_property_id.id
+	
+
+					
+	@api.multi
+	@api.onchange('published_customer','advertising_agency','invoicing_property_id')
+	def onchange_partner_package(self):
+		for line in self:
+			if line.advertising_agency:
+				if line.advertising_agency.invoicing_property_id:
+					if line.invoicing_property_id.inv_package_deal == True:
+						line.package = True
+					else:
+						line.package = False
+			elif line.published_customer:
+				if line.published_customer.invoicing_property_id:
+					if line.invoicing_property_id.inv_package_deal == True:
+						line.package = True
+					else:
+						line.package = False
+			else:
+				if line.invoicing_property_id.inv_package_deal == True:
+						line.package = True
+				else:
+					line.package = False
+	@api.multi
+	@api.onchange('published_customer','advertising_agency','invoicing_property_id')
+	def onchange_partner_invoicing_date(self):
+		for line in self:
+			if line.advertising_agency:
+				if line.advertising_agency.invoicing_property_id:
+					if line.invoicing_property_id.inv_per_line_adv_print == True or line.invoicing_property_id.inv_per_line_adv_online == True:
+						line.inv_date_bool = True
+					else:
+						line.inv_date_bool = False
+			elif line.published_customer:
+				if line.published_customer.invoicing_property_id:
+					if line.invoicing_property_id.inv_per_line_adv_print == True or line.invoicing_property_id.inv_per_line_adv_online == True:
+						line.inv_date_bool = True
+					else:
+						line.inv_date_bool = False
+			else:
+				if line.invoicing_property_id.inv_per_line_adv_print == True or line.invoicing_property_id.inv_per_line_adv_online == True:
+						line.inv_date_bool = True
+				else:
+					line.inv_date_bool = False
+
+	@api.multi
+	@api.onchange('published_customer','advertising_agency','invoicing_property_id')
+	def onchange_partner_pay_terms(self):
+		for line in self:
+			if line.advertising_agency:
+				if line.advertising_agency.invoicing_property_id:
+					if line.invoicing_property_id.pay_in_terms == True:
+						line.terms_cond_bool = True
+					else:
+						line.terms_cond_bool = False
+			elif line.published_customer:
+				if line.published_customer.invoicing_property_id:
+					if line.invoicing_property_id.pay_in_terms == True:
+						line.terms_cond_bool = True
+					else:
+						line.terms_cond_bool = False
+			else:
+				if line.invoicing_property_id.pay_in_terms == True:
+						line.terms_cond_bool = True
+				else:
+					line.terms_cond_bool = False
+
+
+
+
+
+
+class SaleOrderLine(models.Model):
+	_inherit = 'sale.order.line'
+
+	invoicing_property_id = fields.Many2one('invoicing.property',related='order_id.invoicing_property_id',string="Invoicing Property")
+
