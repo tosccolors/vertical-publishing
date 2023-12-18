54648acc23409c3a6b8121c12a250a481420f5d3
Deepa Venkatesh <deepa4lok@yahoo.com>
2023-12-18 14:10:34 +0530

    SAO: package

--- package_sale_advertising_order/__manifest__.py
+++ package_sale_advertising_order/__manifest__.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
+
 {
     'name': "package_sale_advertising_order",
-
     'summary': """
         Package Sale Advertising Order""",
 
     'description': """
         
     """,
-
     'author': 'Magnus - Willem Hulshof',
     'website': 'http://www.magnus.nl',
 
@@ -16,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Sale',
-    'version': '0.1',
+    'version': '14.0.1.1',
 
     # any module necessary for this one to work correctly
     'depends': ['sale_advertising_order'],
