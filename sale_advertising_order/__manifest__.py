cbb7a56f9b6b27e4cf27a6406109467c4713c3ac
Deepa Venkatesh <deepa4lok@yahoo.com>
2024-02-09 19:01:22 +0530

    SAO: Fix default categ

--- sale_advertising_order/__manifest__.py
+++ sale_advertising_order/__manifest__.py
@@ -21,7 +21,7 @@
 ##############################################################################
 {
     'name': 'Sale Advertising Order',
-    'version': '14.0.2.9.3',
+    'version': '14.0.2.9.4',
     'category': 'Sale',
     'description': """
 This module allows you to use both CRM and Sales Management to run your advertising sales
