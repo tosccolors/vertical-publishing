67e1ec69a475460b691c28bee45c1b43ccf7c8e5
Deepa Venkatesh <deepa4lok@yahoo.com>
2024-03-20 17:14:40 +0530

    App renamed: package_sale_advertising_order >> sale_advertising_order

---
+++ sale_advertising_order_package/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Sale Advertising Order Package",
+    'summary': """
+        Package Sale Advertising Order""",
+
+    'description': """
+        
+    """,
+    'author': 'Deepa, Magnus - Willem Hulshof',
+    'website': 'http://www.magnus.nl',
+
+    'category': 'Sale',
+    'version': '14.0.2',
+    'depends': ['sale_advertising_order'],
+    'data': [
+        'views/sale_advertising_view.xml',
+    ],
+    'demo': [],
+}
