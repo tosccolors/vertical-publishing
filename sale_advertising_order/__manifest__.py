d522f71a1d66023f084014cc49bcce6d3a3db654
Deepa Venkatesh <deepa4lok@yahoo.com>
2024-04-11 18:41:24 +0530

    SAO: Access Groups revamp [3]

--- sale_advertising_order/__manifest__.py
+++ sale_advertising_order/__manifest__.py
@@ -21,7 +21,7 @@
 ##############################################################################
 {
     'name': 'Sale Advertising Order',
-    'version': '14.0.4.8.1',
+    'version': '14.0.4.8.2',
     'category': 'Sale',
     'description': """
 This module allows you to use both CRM and Sales Management to run your advertising sales
