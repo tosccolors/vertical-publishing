2375fbab3b991b5a1e4e0d67a480266b8216278f
Deepa Venkatesh <deepa4lok@yahoo.com>
2024-04-18 18:45:51 +0530

    SAO: button create invoice

--- sale_advertising_order/__manifest__.py
+++ sale_advertising_order/__manifest__.py
@@ -21,7 +21,7 @@
 ##############################################################################
 {
     'name': 'Sale Advertising Order',
-    'version': '14.0.5.3',
+    'version': '14.0.5.4',
     'category': 'Sale',
     'description': """
 This module allows you to use both CRM and Sales Management to run your advertising sales
