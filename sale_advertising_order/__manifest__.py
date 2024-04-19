94d47819ff718c3fdc1fd1ebe4ae0afe391c2dc1
Deepa Venkatesh <deepa4lok@yahoo.com>
2024-04-19 15:44:59 +0530

    SAO: sale report finetunes

--- sale_advertising_order/__manifest__.py
+++ sale_advertising_order/__manifest__.py
@@ -21,7 +21,7 @@
 ##############################################################################
 {
     'name': 'Sale Advertising Order',
-    'version': '14.0.5.6',
+    'version': '14.0.5.7',
     'category': 'Sale',
     'description': """
 This module allows you to use both CRM and Sales Management to run your advertising sales
