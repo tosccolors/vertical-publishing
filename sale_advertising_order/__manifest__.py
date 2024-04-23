ba88dca46aee5ab6b511368b3c913a406b8ddcd7
Deepa Venkatesh <deepa4lok@yahoo.com>
2024-04-23 09:15:34 +0530

    SAO: lead fixes

--- sale_advertising_order/__manifest__.py
+++ sale_advertising_order/__manifest__.py
@@ -21,7 +21,7 @@
 ##############################################################################
 {
     'name': 'Sale Advertising Order',
-    'version': '14.0.5.8',
+    'version': '14.0.5.9',
     'category': 'Sale',
     'description': """
 This module allows you to use both CRM and Sales Management to run your advertising sales
