75bebf500e2ba952a0c8bf5cefaee16bd0981a49
deepa-venkatesh <deepa@xfactory.in>
2024-05-24 16:11:54 +0530

    SAO: Invoice finetunes

--- sale_advertising_order/__manifest__.py
+++ sale_advertising_order/__manifest__.py
@@ -21,7 +21,7 @@
 ##############################################################################
 {
     'name': 'Sale Advertising Order',
-    'version': '14.0.10.1',
+    'version': '14.0.10.2',
     'category': 'Sale',
     'description': """
 This module allows you to use both CRM and Sales Management to run your advertising sales
