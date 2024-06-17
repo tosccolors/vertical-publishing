bf335d997d8a8bf5eb22c060b8b3336717a9ebcb
Deepa Venkatesh <deepa4lok@yahoo.com>
2024-06-17 12:20:30 +0530

    SAO: deprecated v10 links on Partner

--- sale_advertising_order/__manifest__.py
+++ sale_advertising_order/__manifest__.py
@@ -21,7 +21,7 @@
 ##############################################################################
 {
     'name': 'Sale Advertising Order',
-    'version': '14.0.10.9',
+    'version': '14.0.11.0',
     'category': 'Sale',
     'description': """
 This module allows you to use both CRM and Sales Management to run your advertising sales
