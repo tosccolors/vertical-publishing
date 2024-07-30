ab3be5b9565f3cbe2967dd6f7331df9be029d367
Deepa Venkatesh <deepa4lok@yahoo.com>
2024-07-30 17:02:41 +0530

    SAO: SOL; onchange; price compute; Issue Products

--- sale_advertising_order/__manifest__.py
+++ sale_advertising_order/__manifest__.py
@@ -22,7 +22,7 @@
 ##############################################################################
 {
     'name': 'Sale Advertising Order',
-    'version': '16.0.0.8',
+    'version': '16.0.0.9',
     'category': 'Sale',
     'description': """
 This module allows you to use Sales Management to run your advertising sales
