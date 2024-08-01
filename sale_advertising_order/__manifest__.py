1980ede50ea617b3b1ae04ca8b8386cd3947e49b
Deepa Venkatesh <deepa4lok@yahoo.com>
2024-08-01 15:25:30 +0530

    SAO: Schedules; Order Analysis

--- sale_advertising_order/__manifest__.py
+++ sale_advertising_order/__manifest__.py
@@ -22,7 +22,7 @@
 ##############################################################################
 {
     'name': 'Sale Advertising Order',
-    'version': '16.0.1.4',
+    'version': '16.0.1.5',
     'category': 'Sale',
     'description': """
 This module allows you to use Sales Management to run your advertising sales
