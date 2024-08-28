8859e87b1423fac607d170ac34de48390490ef4a
Deepa Venkatesh <deepa4lok@yahoo.com>
2024-08-28 14:03:18 +0530

    SAO: SOT hide on partner

--- sale_advertising_order/__manifest__.py
+++ sale_advertising_order/__manifest__.py
@@ -21,7 +21,7 @@
 ##############################################################################
 {
     'name': 'Sale Advertising Order',
-    'version': '14.0.13.7',
+    'version': '14.0.13.8',
     'category': 'Sale',
     'description': """
 This module allows you to use both CRM and Sales Management to run your advertising sales
