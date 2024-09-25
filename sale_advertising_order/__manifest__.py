54d7175e737f307031c8c0ede4b17244ced97fc4
kumarinie <kuamrisushmanie@gmail.com>
2024-09-25 17:26:28 +0530

    [27] Clear domain on proof_number_adv_customer field in sale.order.line

--- sale_advertising_order/__manifest__.py
+++ sale_advertising_order/__manifest__.py
@@ -21,7 +21,7 @@
 ##############################################################################
 {
     'name': 'Sale Advertising Order',
-    'version': '14.0.15.1',
+    'version': '14.0.15.2',
     'category': 'Sale',
     'description': """
 This module allows you to use both CRM and Sales Management to run your advertising sales
