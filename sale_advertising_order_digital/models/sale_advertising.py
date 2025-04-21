962215759b4e9c9bb5708b5f649c5d7312641f9d
Deepa Venkatesh <deepa4lok@yahoo.com>
2025-04-21 09:47:30 +0530

    [639] Archived Adv Issues

--- sale_advertising_order_digital/models/sale_advertising.py
+++ sale_advertising_order_digital/models/sale_advertising.py
@@ -64,7 +64,7 @@ class SaleOrderLine(models.Model):
             # adv_class_issue_ids = ol.ad_class.adv_class_issue_ids
             # class_issue_ids = adv_class_issue_ids and adv_class_issue_ids.ids or []
             titles = ol.title + ol.title_ids
-            domain =[('parent_id', 'in', titles.ids)]
+            domain =[('parent_id', 'in', titles.ids), ('active','=', True)]
             if ol.issue_date_filter:
                 domain += [('issue_date','>=', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))]
             # if class_issue_ids:
