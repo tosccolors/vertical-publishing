df574c9a2255db48abcc189d3cededea249302a8
Deepa Venkatesh <deepa4lok@yahoo.com>
2025-05-29 13:40:57 +0530

    [818][MIG] SAO: disable impossible combo

--- sale_advertising_order/__manifest__.py
+++ sale_advertising_order/__manifest__.py
@@ -21,7 +21,7 @@
 ##############################################################################
 {
     "name": "Sale Advertising Order",
-    "version": "16.0.9.3.0",
+    "version": "16.0.9.4.0",
     "category": "Sale",
     "author": "Deepa Venkatesh (DK), " "Willem Hulsof, The Open Source Company (TOSC)",
     "website": "https://github.com/OCA/l10n-netherlands",
